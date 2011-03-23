f93169b855ae86f7917aa4c79858f278fbf4a76a
Julian Langschaedel <meta.rb@gmail.com>
2011-03-23 08:11:33 +0100

    first timer

--- ffi-libevent.rb
+++ ffi-libevent.rb
@@ -14,9 +14,55 @@ module Event
   attach_function :evbuffer_add_printf, [:pointer, :string], :int
   attach_function :evhttp_send_reply, [:pointer, :int, :string, :pointer], :int
   attach_function :evbuffer_free, [:pointer], :int
+
+
+  attach_function :event_base_new, [], :pointer
+  attach_function :event_base_free, [:pointer], :void
+  attach_function :event_base_get_method, [:pointer], :string
+
+  attach_function :event_base_dispatch, [:pointer], :int
+
+  callback :callback, [:pointer, :pointer], :void
+  attach_function :event_new, [:pointer, :int, :short, :callback], :pointer
+  attach_function :event_add, [:pointer, :pointer], :int
+  attach_function :event_assign, [:pointer, :pointer, :int, :int, :callback], :pointer
+
+  EVLOOP_ONCE     = 1
+  EVLOOP_NONBLOCK = 2
+  EV_TIMEOUT = 1
+  EV_READ = 2
+  EV_PERSIST = 10
+  attach_function :event_base_loop, [:pointer, :int], :int
 end
 
 
+ev1, tv = nil, nil
+
+cb_func = Proc.new{|*a| Event.event_add(ev1, tv); p ['callback', Time.now.tv_sec] }
+
+base = Event.event_base_new
+ev1 = Event.event_new(base, -1, 0, cb_func)
+
+tv = FFI::MemoryPointer.new(:int, 2).put_array_of_int(0, [0, 10])
+Event.event_add(ev1, tv)
+
+Event.event_base_dispatch(base)
+#10.times{ Event.event_base_loop(base, 2); sleep(1.2) }
+
+
+__END__
+p base = Event.event_base_new
+p Event.event_base_get_method(base)
+
+2.times{
+  #p Event.event_base_loop(base, 2)
+
+  p Event.event_base_loop(base, 0)
+}
+
+Event.event_base_free(base)
+
+__END__
 if $0 == __FILE__
   Event.event_init
 
